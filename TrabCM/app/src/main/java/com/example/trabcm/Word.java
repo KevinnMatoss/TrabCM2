2766169a5d6ef933c1ad0825d366ee30b222373d
KevinnMatoss <kmatoscerqueira@gmail.com>
2020-03-29 15:35:38 +0100

    Criação da classe Word

--- TrabCM/app/src/main/java/com/example/trabcm/Word.java
+++ TrabCM/app/src/main/java/com/example/trabcm/Word.java
@@ -3,7 +3,17 @@ package com.example.trabcm;
 import androidx.annotation.NonNull;
 
 public class Word {
-private String mWord;
-public Word(@NonNull String word) {this.mWord = word;}
-public String getWord(){return this.mWord;}
+    private String mWord;
+    public Word(@NonNull String word) {this.mWord = word;}
+    public String getWord(){return this.mWord;}
+}
+
+@Entity(tableName = "word_table")
+public class Word{
+    @PrimaryKey
+    @NonNull
+    @CollumnInfo(name = "word")
+    private String mWord;
+
+    public Word(@NonNull Stro)
 }
